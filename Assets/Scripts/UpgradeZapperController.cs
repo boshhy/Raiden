3af7df58702841661db98eab03a7caf6e64502fb
boshhy <boshhy101@yahoo.com>
2023-08-09 20:39:28 -0700

    Added constant laser animation

--- Assets/Scripts/UpgradeZapperController.cs
+++ Assets/Scripts/UpgradeZapperController.cs
@@ -21,6 +21,7 @@ public class UpgradeZapperController : MonoBehaviour
         
     }
 
+    // Used to Spawn a lazer
     private void FireTheLazer()
     {
         Vector3 theBulletPosition = gameObject.transform.position;
@@ -28,27 +29,38 @@ public class UpgradeZapperController : MonoBehaviour
         Instantiate(bulletZapper, theBulletPosition, gameObject.transform.rotation);  
     }
 
-    private void FireZapperGun(InputAction.CallbackContext cntx)
+    // used to change animation to "always fire"
+    private void ChangeAnimToKeepFiring()
     {
-        anim.SetBool("isFiringZapper", true);
+        Debug.Log("in the change animation function");
+        anim.SetInteger("zapperAnimation", 2);
     }
 
+    // start firing gun
+    private void StartFiringZapperGun(InputAction.CallbackContext cntx)
+    {
+        Debug.Log("in the startfire function");
+        anim.SetInteger("zapperAnimation", 1);
+    }
+
+    // Stop firing gun
     private void StopFiringZapperGun(InputAction.CallbackContext cntx)
     {
-        anim.SetBool("isFiringZapper", false);
+        Debug.Log("in the stop firing function");
+        anim.SetInteger("zapperAnimation", 0);
     }
 
     private void OnEnable()
     {
         zapperController.Ship.Enable();
-        zapperController.Ship.FireZapperGun.performed += FireZapperGun;
+        zapperController.Ship.FireZapperGun.started += StartFiringZapperGun;
         zapperController.Ship.FireZapperGun.canceled += StopFiringZapperGun;
     }
 
     private void OnDisable()
     {
         zapperController.Ship.Disable();
-        zapperController.Ship.FireZapperGun.performed -= FireZapperGun;
+        zapperController.Ship.FireZapperGun.started -= StartFiringZapperGun;
         zapperController.Ship.FireZapperGun.canceled -= StopFiringZapperGun;
     }
 }
